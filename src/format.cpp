3a18039d81e720119fe2eed5dcf37ffafcb61de5
Anas Aqeel <49162254+anasaqeel@users.noreply.github.com>
2022-08-27 23:47:04 -0400

    Update format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -1,5 +1,6 @@
 #include <string>
-
+#include <sstream>
+#include <iomanip>
 #include "format.h"
 
 using std::string;
@@ -8,4 +9,22 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) 
+{
+    std::stringstream ss;
+    string time;
+    long hour = 0;
+    long min = 0;
+    long sec = 0;
+
+    min = sec/60;
+    hour = min/60;
+
+    sec = seconds - (min*60);
+    min = min - (hour*60);
+
+    ss << std::setw(2) << std::setfill('0') << hour << ":" << std::setw(2) << std::setfill('0') << min << ":" << std::setw(2) << std::setfill('0') << sec;
+    time = ss.str();
+    return time;
+    
+}
